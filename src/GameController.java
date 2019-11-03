ef57443da35a5ac22aaddf5470fe306b6d07fda4
pietrzakkuba <jkapietrzak@gmail.com>
2019-11-03 15:39:40 +0100

    added recognizing game over

--- src/GameController.java
+++ src/GameController.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
@@ -22,6 +24,8 @@ public class GameController implements Initializable {
     public TextArea Console;
     public Pane pane;
     private boolean isX;
+    private String[] values = new String[9];
+
 
     private boolean myTurn() throws IOException {
         String turn = Main.readFromServer();
@@ -38,7 +42,65 @@ public class GameController implements Initializable {
     }
 
     private void checkState() {
-        
+
+        if
+        (
+            values[0].equals(values[1]) &&
+            values[0].equals(values[2]) &&
+            !values[0].equals("")
+        ) {
+            System.out.println("GAME OVER 123");
+        } else if
+        (
+           values[3].equals(values[4]) &&
+            values[3].equals(values[5]) &&
+            !values[3].equals("")
+        ) {
+            System.out.println("GAME OVER 456");
+        } else if
+        (
+            values[6].equals(values[7]) &&
+            values[6].equals(values[8]) &&
+            !values[6].equals("")
+        ) {
+            System.out.println("GAME OVER 789");
+        } else if
+        (
+            values[0].equals(values[3]) &&
+            values[0].equals(values[6]) &&
+            !values[0].equals("")
+        ) {
+            System.out.println("GAME OVER 147");
+        } else if
+        (
+            values[1].equals(values[4]) &&
+            values[1].equals(values[7]) &&
+            !values[1].equals("")
+        ) {
+            System.out.println("GAME OVER 258");
+        } else if
+        (
+            values[2].equals(values[5]) &&
+            values[2].equals(values[8]) &&
+            !values[2].equals("")
+        ) {
+            System.out.println("GAME OVER 369");
+        } else if
+        (
+            values[0].equals(values[4]) &&
+            values[0].equals(values[8]) &&
+            !values[0].equals("")
+        ) {
+            System.out.println("GAME OVER 159");
+        } else if
+        (
+            values[2].equals(values[4]) &&
+            values[2].equals(values[6]) &&
+            !values[2].equals("")
+        ) {
+            System.out.println("GAME OVER 357");
+        }
+
     }
 
     private void changeAbility(boolean my_turn) {
@@ -48,7 +110,10 @@ public class GameController implements Initializable {
     private void waitForMove() throws IOException {
         Button button;
         String message_from_server = Main.readFromServer();
-        String button_id = "button" + message_from_server.charAt(1);
+        char button_number = message_from_server.charAt(1);
+        String button_id = "button" + button_number;
+        values[button_number - '0' - 1] = isX ? "O" : "X";
+        checkState();
         button = (Button)pane.getScene().lookup("#" + button_id);
         Platform.runLater ( () -> {
             button.setDisable(true);
@@ -61,7 +126,7 @@ public class GameController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        new Thread(()->{
+        new Thread( ()->{
             try {
                 if (myTurn()) {
                     LabelYou.setText("X");
@@ -79,6 +144,11 @@ public class GameController implements Initializable {
                 e.printStackTrace();
             }
         }).start();
+        new Thread( ()->{
+           for(int i=0; i<9; i++){
+               values[i] = "";
+           }
+        }).start();
     }
 
 
@@ -92,7 +162,10 @@ public class GameController implements Initializable {
                 button.setText(isX ? "X" : "O");
             });
             String message_to_server = button.getId();
-            message_to_server = first_number + message_to_server.charAt(message_to_server.length() - 1) + '\0';
+            char button_number = message_to_server.charAt(message_to_server.length() - 1);
+            values[button_number - '0' - 1] = isX ? "X" : "O";
+            checkState();
+            message_to_server = first_number + button_number + '\0';
             if (!Console.getText().equals("")) Console.appendText("\n");
                 Console.appendText("I am sending this to server:  " + message_to_server);
             try {
